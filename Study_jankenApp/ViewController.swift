3e6338c85019f7bd995ebe6770e811728e250c9c
Daichi Hayashi <daichikuwa0618@gmail.com>
2017-09-14 20:17:20 +0900

    Rotated computer image in 180deg

--- Study_jankenApp/ViewController.swift
+++ Study_jankenApp/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var computerImageView: UIImageView!
+    @IBOutlet weak var playerImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //180度のラジアンを求める
+        let angle:CGFloat = CGFloat((180.0 * CGFloat.pi) / 180.0)
+        //イメージビューの回転
+        computerImageView.transform = CGAffineTransform(rotationAngle: angle)
     }
 
     override func didReceiveMemoryWarning() {
