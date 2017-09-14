771c78637f2ff3fd64254474fb77c55874edfc24
Daichi Hayashi <daichikuwa0618@gmail.com>
2017-09-14 19:04:03 +0900

    Initial Commit

---
+++ Study_jankenApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study_jankenApp
+//
+//  Created by Daichi Hayashi on 2017/09/14.
+//  Copyright © 2017年 Daichi Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
