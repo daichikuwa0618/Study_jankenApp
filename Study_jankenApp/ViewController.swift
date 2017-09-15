617b4789977bb9a446d0e0bb8ea42a0a68705f9f
Daichi Hayashi <daichikuwa0618@gmail.com>
2017-09-15 09:10:11 +0900

    Done

--- Study_jankenApp/ViewController.swift
+++ Study_jankenApp/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var computerImageView: UIImageView!
     @IBOutlet weak var playerImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
@@ -28,6 +29,80 @@ class ViewController: UIViewController {
         playerImageView.image = UIImage(named: "gu.jpg")
         // ラベルに「じゃんけん！」と表示する
         messageLabel.text = "じゃんけん！"
+    }
+    @IBAction func tapGu() {
+        playerImageView.image = UIImage(named: "gu.jpg")
+        doComputer(0)
+    }
+    @IBAction func tapChoki() {
+        playerImageView.image = UIImage(named: "choki.jpg")
+        doComputer(1)
+    }
+    @IBAction func tapPa() {
+        playerImageView.image = UIImage(named: "pa.jpg")
+        doComputer(2)
+    }
+    
+    func doComputer(_ player:Int) {
+        // コンピュータの手を0から2に割り当てて求める
+        let computer = arc4random_uniform(3)
+        // 勝敗表示用の文字列
+        var msg = ""
+        
+        switch computer {
+        case 0:
+            // グー
+            computerImageView.image = UIImage(named: "gu.jpg")
+            switch player {
+            case 0:
+                //グー
+                msg = "あいこ！"
+            case 1:
+                //チョキ
+                msg = "負け！"
+            case 2:
+                //パー
+                msg = "勝ち！"
+            default:
+                break
+            }
+        case 1:
+            // チョキ
+            computerImageView.image = UIImage(named: "choki.jpg")
+            switch player {
+            case 0:
+                //グー
+                msg = "勝ち！"
+            case 1:
+                //チョキ
+                msg = "あいこ！"
+            case 2:
+                //パー
+                msg = "負け！"
+            default:
+                break
+            }
+        case 2:
+            // パー
+            computerImageView.image = UIImage(named: "pa.jpg")
+            switch player {
+            case 0:
+                //グー
+                msg = "負け！"
+            case 1:
+                //チョキ
+                msg = "勝ち！"
+            case 2:
+                //パー
+                msg = "あいこ！"
+            default:
+                break
+            }
+        default:
+            break
+        }
+        
+        messageLabel.text = msg
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
